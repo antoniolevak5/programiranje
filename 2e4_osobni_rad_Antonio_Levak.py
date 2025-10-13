af52b93c7cc988b5a0b52a506670b774eb4311b4
Antonio Levak <antoniolevak10@gmail.com>
2025-10-13 19:27:25 +0200

    Update 2e4_osobni_rad_Antonio_Levak.py

--- 2e4_osobni_rad_Antonio_Levak.py
+++ 2e4_osobni_rad_Antonio_Levak.py
@@ -13,17 +13,17 @@ while True:
     if opcija == 1:   # == != < > <> >= <=
         print("Pretvarač volta(V) u milivolte(mV)")
         volt = float(input("Upiši napon (u V):"))
-        mV = volt/1000
+        mV = volt*1000
         print(f"Napon je: {mV} mV") #Ispis rezultata
     elif opcija == 2:
         print("Pretvarač ohma(Ω) u kiloohme(kΩ)")
         ohm = float(input("Upiši otpor (u Ω):"))
-        kΩ = ohm*1000
+        kΩ = ohm/1000
         print(f"Otpor je: {kΩ} kΩ")
     elif opcija == 3:
         print("Pretvarač ampera(A) u miliampere(mA)")
         amper = float(input("Upiši jakost struje (u A):"))
-        mA = amper/1000
+        mA = amper*1000
         print(f"Jakost struje je: {mA} mA")
     elif opcija == 0:
         print("Hvala na korištenju. Doviđenja!")
